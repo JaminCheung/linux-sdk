a60625beeb793a7e2977045a4bb8fe98878ce7a3
ZhangYanMing <yanming.zhang@ingenic.com>
2017-07-05 13:01:09 +0800

    battery: more bat info
    Change-Id: Ieb4a3c27bddae38b8b17f2506f838d60f91c4ddb

--- battery/battery_manager.c
+++ battery/battery_manager.c
@@ -38,20 +38,19 @@ struct listener {
     struct list_head head;
 };
 
-static const char* state2str(uint8_t state) {
-    switch (state) {
-    case POWER_SUPPLY_STATUS_CHARGING:
-        return "Charging";
-    case POWER_SUPPLY_STATUS_DISCHARGING:
-        return "Discharging";
-    case POWER_SUPPLY_STATUS_NOT_CHARGING:
-        return "Charging";
-    case POWER_SUPPLY_STATUS_FULL:
-        return "Full";
-    default:
-        return "Unknown";
-    }
-}
+static char *health_strs[] = {
+        "Unknown", "Good", "Overheat", "Dead", "Over voltage",
+        "Unspecified failure", "Cold", "Watchdog timer expire",
+        "Safety timer expire"
+};
+static char *technology_strs[] = {
+        "Unknown", "NiMH", "Li-ion", "Li-poly", "LiFe", "NiCd",
+        "LiMn"
+};
+
+static char *status_strs[] = {
+        "Unknown", "Charging", "Discharging", "Not charging", "Full"
+};
 
 static void handle_power_supply_event(struct netlink_handler* nh,
         struct netlink_event* event) {
@@ -63,6 +62,12 @@ static void handle_power_supply_event(struct netlink_handler* nh,
 
     const char* state = event->find_param(event, "POWER_SUPPLY_STATUS");
     const char* cap = event->find_param(event, "POWER_SUPPLY_CAPACITY");
+    const char* vol_now = event->find_param(event, "POWER_SUPPLY_VOLTAGE_NOW");
+    const char* vol_max = event->find_param(event, "POWER_SUPPLY_VOLTAGE_MAX_DESIGN");
+    const char* vol_min = event->find_param(event, "POWER_SUPPLY_VOLTAGE_MIN_DESIGN");
+    const char* tech = event->find_param(event, "POWER_SUPPLY_TECHNOLOGY");
+    const char* health = event->find_param(event, "POWER_SUPPLY_HEALTH");
+    const char* present = event->find_param(event, "POWER_SUPPLY_PRESENT");
 
     struct battery_event *battery_event = calloc(1,
             sizeof(struct battery_event));
@@ -80,7 +85,45 @@ static void handle_power_supply_event(struct netlink_handler* nh,
             battery_event->state = POWER_SUPPLY_STATUS_UNKNOWN;
     }
 
+    if (!strcmp(tech, "NiMH"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_NiMH;
+    else if (!strcmp(tech, "Li-ion"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_LION;
+    else if (!strcmp(tech, "Li-poly"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_LIPO;
+    else if (!strcmp(tech, "LiFe"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_LiFe;
+    else if (!strcmp(tech, "NiCd"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_NiCd;
+    else if (!strcmp(tech, "LiMn"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_LiMn;
+    else if (!strcmp(tech, "Unknown"))
+        battery_event->technology = POWER_SUPPLY_TECHNOLOGY_UNKNOWN;
+
+    if (!strcmp(health, "Good"))
+        battery_event->health = POWER_SUPPLY_HEALTH_GOOD;
+    else if (!strcmp(health, "Overheat"))
+        battery_event->health = POWER_SUPPLY_HEALTH_OVERHEAT;
+    else if (!strcmp(health, "Dead"))
+        battery_event->health = POWER_SUPPLY_HEALTH_DEAD;
+    else if (!strcmp(health, "Over voltage"))
+        battery_event->health = POWER_SUPPLY_HEALTH_OVERVOLTAGE;
+    else if (!strcmp(health, "Unspecified failure"))
+        battery_event->health = POWER_SUPPLY_HEALTH_UNSPEC_FAILURE;
+    else if (!strcmp(health, "Cold"))
+        battery_event->health = POWER_SUPPLY_HEALTH_COLD;
+    else if (!strcmp(health, "Watchdog timer expire"))
+        battery_event->health = POWER_SUPPLY_HEALTH_WATCHDOG_TIMER_EXPIRE;
+    else if (!strcmp(health, "Safety timer expire"))
+        battery_event->health = POWER_SUPPLY_HEALTH_SAFETY_TIMER_EXPIRE;
+    else if (!strcmp(health, "Unknown"))
+        battery_event->health = POWER_SUPPLY_HEALTH_UNKNOWN;
+
     battery_event->capacity = strtol(cap, NULL, 0);
+    battery_event->voltage_now = strtol(vol_now, NULL, 0);
+    battery_event->voltage_max = strtol(vol_max, NULL, 0);
+    battery_event->voltage_min = strtol(vol_min, NULL, 0);
+    battery_event->present = strtol(present, NULL, 0);
 
     pthread_mutex_lock(&listener_lock);
 
@@ -195,8 +238,14 @@ static void unregister_event_listener(battery_event_listener_t listener) {
 static void dump_event(struct battery_event* event) {
     LOGI("========================================\n");
     LOGI("Dump battery event\n");
-    LOGI("State: %s\n", state2str(event->state));
-    LOGI("Capacity: %u\n", event->capacity);
+    LOGI("Technology:  %s\n", technology_strs[event->technology]);
+    LOGI("Present:     %u\n", event->present);
+    LOGI("Health:      %s\n", health_strs[event->health]);
+    LOGI("State:       %s\n", status_strs[event->state]);
+    LOGI("Voltage Max: %u\n", event->voltage_max);
+    LOGI("Voltage Min: %u\n", event->voltage_min);
+    LOGI("Voltage Now: %u\n", event->voltage_now);
+    LOGI("Capacity:    %u\n", event->capacity);
     LOGI("========================================\n");
 }
 
--- include/battery/battery_manager.h
+++ include/battery/battery_manager.h
@@ -27,9 +27,37 @@ enum {
         POWER_SUPPLY_STATUS_FULL,
 };
 
+enum {
+    POWER_SUPPLY_TECHNOLOGY_UNKNOWN = 0,
+    POWER_SUPPLY_TECHNOLOGY_NiMH,
+    POWER_SUPPLY_TECHNOLOGY_LION,
+    POWER_SUPPLY_TECHNOLOGY_LIPO,
+    POWER_SUPPLY_TECHNOLOGY_LiFe,
+    POWER_SUPPLY_TECHNOLOGY_NiCd,
+    POWER_SUPPLY_TECHNOLOGY_LiMn,
+};
+
+enum {
+    POWER_SUPPLY_HEALTH_UNKNOWN = 0,
+    POWER_SUPPLY_HEALTH_GOOD,
+    POWER_SUPPLY_HEALTH_OVERHEAT,
+    POWER_SUPPLY_HEALTH_DEAD,
+    POWER_SUPPLY_HEALTH_OVERVOLTAGE,
+    POWER_SUPPLY_HEALTH_UNSPEC_FAILURE,
+    POWER_SUPPLY_HEALTH_COLD,
+    POWER_SUPPLY_HEALTH_WATCHDOG_TIMER_EXPIRE,
+    POWER_SUPPLY_HEALTH_SAFETY_TIMER_EXPIRE,
+};
+
 struct battery_event {
     uint8_t state;
     uint32_t capacity;
+    uint32_t voltage_now;
+    uint32_t voltage_max;
+    uint32_t voltage_min;
+    uint32_t present;
+    uint32_t technology;
+    uint32_t health;
 };
 
 typedef void (*battery_event_listener_t)(struct battery_event *event);
